f478e080d04e4cf54f16dbd79d340f6c846c9d57
levintoo <tooklevin@gmail.com>
2025-05-26 18:57:13 +0300

    feat(inertia): install and configure inertia with react

---
+++ resources/js/pages/app.tsx
@@ -0,0 +1,10 @@
+import {Head} from "@inertiajs/react";
+
+export default function App() {
+    return (
+        <>
+            <Head title="Home" />
+            <div>Hello</div>
+        </>
+    )
+}
